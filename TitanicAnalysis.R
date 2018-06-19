3e6b6446f479ea280503b2fd68490cb66f7c181a
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-06-19 21:15:07 +0200

    Convert dataframe to Tibble

--- TitanicAnalysis.R
+++ TitanicAnalysis.R
@@ -1,5 +1,16 @@
 library(dplyr)
-
-
-
 #setwd("")
+#library(nycflights13)
+#data("flights")
+
+
+# loading flight data
+load("hflights/data/hflights.rda")
+#converting to tibble
+hflights <- hflights %>%  as_data_frame() 
+summary(flights)
+
+getwd()
+
+
+
