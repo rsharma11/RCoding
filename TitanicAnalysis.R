f6eb1432d48a29fef262df8fc9ac72b933b6faa0
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-06-20 21:38:05 +0200

    Usage of Filter

--- TitanicAnalysis.R
+++ TitanicAnalysis.R
@@ -8,9 +8,35 @@ library(dplyr)
 load("hflights/data/hflights.rda")
 #converting to tibble
 hflights <- hflights %>%  as_data_frame() 
-summary(flights)
-
+#summary(flights)
 getwd()
+View(hflights)
+View(planes)
+View(flights)
+Summary(flights)
+class(flights)
+flights
 
+#usage of filter
+#To SFO or OAK
+a<-filter(flights,flights$dest=="SFO")
+View(a)
+b<-filter(flights,flights$dest %in% c("SFO","OAK"))
+View(b)
 
+#In January
+c<-filter(flights,flights$month==1)
+View(c)
+
+#Delayed by more than an hour
+d<-filter(flights,flights$dep_delay>60)
+View(d)
+
+#Departure between midnight and five am
+e<-filter(flights,flights$dep_time )
+View(e)
+
+#Delayed by more than an hour
+f<-filter(flights,flights$arr_delay>2*flights$dep_delay)
+View(f)
 
