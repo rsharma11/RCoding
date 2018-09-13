ce7101f06b9cf9ff88683d3aa343f95ebf7ea7d1
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-09-13 23:05:42 +0200

    CharacterVector

--- CharacterVector.R
+++ CharacterVector.R
@@ -5,3 +5,21 @@ nchar(x)
 
 
 x <- c ("Nature’s", "Best ")
+nchar(x)
+
+x <- c("Nature’s"," At its best ")
+nchar(x)
+
+fname <- "Steve"
+lname <- "Johnson"
+cat(fname,lname)
+
+m <- "Capital of America is Washington"
+substr(m, start=1, stop=18)
+
+n<-"Success is not final, failed is not fatal"
+sub(pattern = "failed", replacement = "failure", x = n)
+
+Names <- c("John", "Andrew", "Thomas")
+Designation <- c("Manager", "Project Head", "Marketing Head")
+data.frame(Names, Designation)
