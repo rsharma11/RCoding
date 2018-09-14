670cd4008cf835f68b5021ffd4f04515e71e502b
ranusharma09@gmail.com <ranusharma09@gmail.com>
2018-09-15 00:01:33 +0200

    lapplyfunction

--- lapply.R
+++ lapply.R
@@ -1,1 +1,32 @@
+#Usage of lapply
+lapply(LIST, FUNCTION, ...)
+
 list1 <- list(observationA = c(1:5, 7:3), observationB=matrix(1:6, nrow=2))
+
+#length of the list
+lapply(list1, length)
+
+#sum of the list
+lapply(list1, sum)
+
+#quantiles of the list
+lapply(list1, quantile)
+
+#class of list1
+lapply(list1, class)
+
+#DerivativeFunction
+DerivativeFunction <- function(x) { log10(x) + 1 }
+lapply(list1, DerivativeFunction)
+
+#unique values of list1
+lapply(list1, unique)
+
+#range of list1
+lapply(list1, range)
+
+#print list1
+lapply(list1, print)
+
+#Convert the above output to a vector
+unlist(lapply(list1, print))
